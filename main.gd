7aac14161de013f4412ce1882c7ee5b0f701a3df
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-01-30 23:53:04 +0300

    Refactor main.gd to set up grid parameters
    Removed old variable and function definitions, added grid settings.

--- main.gd
+++ main.gd
@@ -1,23 +1,6 @@
-var age = 24;
-var sex = male;
-func _ready():
-	var n = 6
-	if n == 6: print("n is equal to six")
-n = 4
-	# Regular 'if' statement 
-	if n == 4:
-		print("n is equal to four")
-if n == 6:
-		print("n is equal to six")
-	else:
-		print("n is not equal to six")
-if n == 6:
-		print("n is equal to six")
-	else:
-		if n < 6:
-			print("n is less than six")
-		else:
-			print("n is greater than six")
-	
-	n = 8
-print("age");
+extends Node2D
+
+# Grid settings
+const GRID_SIZE := 20
+const WIDTH := 32
+const HEIGHT := 24
