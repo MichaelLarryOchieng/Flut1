4c192ddc982a14b527688818944be562c1fcb40f
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-01-30 23:53:58 +0300

    Add initial variables for snake game

--- main.gd
+++ main.gd
@@ -10,3 +10,9 @@ const UP := Vector2(0, -1)
 const DOWN := Vector2(0, 1)
 const LEFT := Vector2(-1, 0)
 const RIGHT := Vector2(1, 0)
+
+var direction := RIGHT
+var snake := []
+var food := Vector2.ZERO
+var move_timer := 0.0
+var move_delay := 0.15
