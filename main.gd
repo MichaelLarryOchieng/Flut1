d829b81ea9a24b10def02bfbd03dd6aa88915f6d
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-01-30 23:53:30 +0300

    Add direction constants for movement

--- main.gd
+++ main.gd
@@ -4,3 +4,9 @@ extends Node2D
 const GRID_SIZE := 20
 const WIDTH := 32
 const HEIGHT := 24
+
+# Directions
+const UP := Vector2(0, -1)
+const DOWN := Vector2(0, 1)
+const LEFT := Vector2(-1, 0)
+const RIGHT := Vector2(1, 0)
