afc4ebcf20a81f7fcbab7c68ffe5eb050a80ad59
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-02-05 23:50:32 +0300

    Add vibration feedback on up button press

--- game.gd
+++ game.gd
@@ -14,4 +14,8 @@ Input.vibrate_handheld(duration_ms)
 func vibrate(ms := 30):
 	if OS.has_feature("mobile"):
 		Input.vibrate_handheld(ms)
+func _on_up_button_pressed():
+	if direction != DOWN:
+		direction = UP
+		vibrate(20)
 
