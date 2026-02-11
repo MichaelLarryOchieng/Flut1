b26ef8f1f87d204c485621bc9bd103ac5e94568c
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-02-11 23:38:22 +0300

    Add directional constants for movement

--- game.gd
+++ game.gd
@@ -1,6 +1,11 @@
 const GRID_SIZE := 20
 const WIDTH := 32
 const HEIGHT := 24
+
+const UP := Vector2(0, -1)
+const DOWN := Vector2(0, 1)
+const LEFT := Vector2(-1, 0)
+const RIGHT := Vector2(1, 0)
 	
 	
 
