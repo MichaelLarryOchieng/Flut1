876ed245abea1e3489fd70f3ef8120472aa6cf16
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-02-11 23:37:57 +0300

    Refactor game.gd by removing unused variables and functions
    Removed score and combo variables along with related button press functions.

--- game.gd
+++ game.gd
@@ -1,39 +1,7 @@
-var score := 0
-var combo_count := 0
-var combo_timer := 0.0
-var combo_window := 2.0 # seconds allowed between foods
-
-func _on_up_button_pressed():
-	if direction != DOWN:
-		direction = UP
-func _on_down_button_pressed():
-	if direction != UP:
-		direction = DOWN
-func _on_left_button_pressed():
-	if direction != RIGHT:
-		direction = LEFT
-func _on_right_button_pressed():
-	if direction != LEFT:
-		direction = RIGHT
-Input.vibrate_handheld(duration_ms)
-func vibrate(ms := 30):
-	if OS.has_feature("mobile"):
-		Input.vibrate_handheld(ms)
-func _on_up_button_pressed():
-	if direction != DOWN:
-		direction = UP
-		vibrate(20)
-func _on_down_button_pressed():
-	if direction != UP:
-		direction = DOWN
-		vibrate(20)
-func _on_left_button_pressed():
-	if direction != RIGHT:
-		direction = LEFT
-		vibrate(20)
-func _on_right_button_pressed():
-	if direction != LEFT:
-		direction = RIGHT
-		vibrate(20)
+const GRID_SIZE := 20
+const WIDTH := 32
+const HEIGHT := 24
+	
+	
 
 
