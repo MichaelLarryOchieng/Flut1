99707d4be7724bfb3847a047abce01a8eb7d836b
MichaelLarryOchieng <ochiengmichaellarry@gmail.com>
2026-03-31 23:03:25 +0300

    Update main.gd

--- main.gd
+++ main.gd
@@ -12,8 +12,7 @@ const LEFT := Vector2(-1, 0)
 const RIGHT := Vector2(1, 0)
 
 
-# Directions
-const UP := Vector2(0, -1)
+
 const DOWN := Vector2(0, 1)
 const LEFT := Vector2(-1, 0)
 const RIGHT := Vector2(1, 0)
